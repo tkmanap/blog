6b652b0a698761f05521833a13ed064578931d0f
manaptk <tk.manap@gmail.com>
2025-01-24 11:41:03 +0300

    1.2 Верстка, material ui, scss

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,20 +1,19 @@
-import {Link, Route, Routes} from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import Container from "@mui/material/Container";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
 
 function App() {
     return (
         <>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Routes>
+            <Header />
+            <Container maxWidth="lg">
+                <Home />
+                {/*<FullPost />*/}
+                {/*<AddPost />*/}
+                {/*<Login />*/}
+                {/*<Registration />*/}
+            </Container>
         </>
     );
 }
